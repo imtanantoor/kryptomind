f91704f2883628e7549f8f3541940225097eb9d4
Abubakar <abubakar.codingpixel@gmail.com>
2020-04-24 15:30:41 +0500

    no message

--- functions.js
+++ functions.js
@@ -13,3 +13,7 @@ function toggleHeader() {
 function alertData() {
   alert("Hello");
 }
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
